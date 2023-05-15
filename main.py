5d02e4415c9f969a440235c6921377798074fc00
Maya <fox>
2023-05-15 12:45:28 +0200

    commit prueba 2

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import pideunnumero
 # print(numero_entero)
 
 numero_entero = pideunnumero.pideUnNum()
-print(numero_entero)
+print(numero_entero)def main():
+   numero_entero = pideunnumero.pideUnNum()
+   valor_lista = elegirelementolista.elegirValor()
